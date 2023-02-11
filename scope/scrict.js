9fef07537d08ab65cf86dff309fc0ea2970e8ae4
Antonio Jimenez <yisusxd@Jesus_A.localdomain>
2023-02-10 20:49:19 -0600

    Notes about strict mode

---
+++ scope/scrict.js
@@ -0,0 +1,10 @@
+"use strict";
+let pi = 3.1415;
+console.log(pi);
+
+const myFunction = function() 
+    {
+        "use strict";
+        return pi = 13.1415;
+        // No va a correr la función porque estoy usando el modo stricto
+    }
